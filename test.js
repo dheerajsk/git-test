b299e01c50c289c8db5c4f3d45133d34eb083b79
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-08-05 22:50:25 +0530

    Next commit

--- test.js
+++ test.js
@@ -4,3 +4,9 @@ console.log("Test");
 // 2. Check changes
 // 3. Commit changes
     // save your changes to repository
+// by here, you code is still on your machine.
+
+// github is online cloud storegae to save your code.
+
+// 4. Publish/Push your changes. on github
+    // 
